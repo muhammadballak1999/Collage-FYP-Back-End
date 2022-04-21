e462b5cd919bb0724da0d481cd6d47d84feab5b6
muhammadballak <muhammadballak1999@gmail.com>
2022-04-20 22:37:50 -0700

    Otp sign up resend has been added

--- routes/authentication.js
+++ routes/authentication.js
@@ -6,7 +6,7 @@ const Validator = require("../middlewares/validator/validator");
 router.post('/login', Validator(authValidators.login), auth.login);
 router.get('/otp/:phone', auth.otp);
 router.post('/otp-sign-up', Validator(authValidators.otpSignUp), auth.otpSignup);
-router.get('/otp-sign-up/resend', Validator(authValidators.otpSignUp), auth.otpSignupResend);
+router.post('/otp-sign-up/resend', Validator(authValidators.otpSignUp), auth.otpSignupResend);
 router.post('/otp-sign-up-verify', Validator(authValidators.otpSignUpVerify), auth.otpSignUpVerify);
 router.get('/verify-otp/:phone/:otp', auth.verify_otp);
 
